49c4ffd3ce8237221289f63db2e847f453698025
velvetvi123 <vic_velvet@yahoo.com>
2024-06-06 11:23:07 -0700

    changing

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,17 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - Deletes an entire binary tree
- * @tree: Pointer to the root node of the tree to delete
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree: pointer to the root node of the tree to delete
  *
- * Return: Nothing
+ * Return: nothing
  */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree)
-    {
-        binary_tree_delete(tree->left);  /* Recursively delete the left subtree */
-        binary_tree_delete(tree->right); /* Recursively delete the right subtree */
-        free(tree);                      /* Free the root node */
-    }
+	if (tree)
+	{
+		binary_tree_delete(tree->left); /* Recursively delete the left subtree */
+		binary_tree_delete(tree->right); /* Recursively delete the right subtree */
+		free(tree); /* Free the root node */
+	}
+}
