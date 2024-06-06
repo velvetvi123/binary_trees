667bef55b6ce10dca558e95490ef26278257a918
velvetvi123 <vic_velvet@yahoo.com>
2024-06-06 09:07:11 -0700

    0 file

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,40 +1,23 @@
-#include "search_algos.h"
+#include "binary_trees.h"
 
 /**
+ * binary_tree_node - Create a binary node
+ * @parent: node parent
+ * @value: Value of the node
+ * Return: the new node
+ */
 
-linear_search - Uses Linear search algorithm to find a value in an array of integers.
-
-@array: Pointer to the first element of the array to search.
-
-@size: Number of elements in the array.
-
-@value: Value to search for.
-
-Return: The index of the first occurrence of ‘value’ in ‘array’, or -1 if ‘value’ is not found or ‘array’ is NULL.
-
-    Prints each element compared with ‘value’ during searching process. */
-Copy
-int linear_search(int *array, size_t size, int value)
-
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_node;
 
-size_t i; // Loop variable
-
-
-
-for (i = 0; array && i < size; ++i) // Iterate over each element in the array
-
-{
-
-    printf("Value checked array[%lu] = [%d]\n", i, array[i]); // Print out what values are being checked
-
-    if (array[i] == value) // Check if value of each current item is same as “value”
-
-        return (i); // Give back index where “value” was located
-
-}
-
-
-
-return (-1); // If no match found then change it to -1
-}
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return (new_node);
